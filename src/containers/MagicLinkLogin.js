21cd4459002058399fe8dae5ceb87018f7b449f4
sqrtofsaturn <sqrtofsaturn@gmail.com>
2017-05-12 14:03:32 -0700

    be more like an authenticator

--- src/containers/MagicLinkLogin.js
+++ src/containers/MagicLinkLogin.js
@@ -1,20 +1,28 @@
 import React from "react"
+import get from 'lodash/get'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
 import MagicLinkLoginPage from "../components/MagicLinkLoginPage"
-import { generateLink } from "../ducks/magic-link"
+import { generateLink, setDomain, setLoginUrl } from "../ducks/magic-link"
 
 const propTypes = {
   dispatch: PropTypes.func.isRequired,
   busy: PropTypes.bool,
   error: PropTypes.object,
+  loginUrl: PropTypes.string,
+  domain: PropTypes.string.isRequired,
 }
 
 class MagicLinkLogin extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(setDomain(get(this.props, "location.query.domain")))
+    this.props.dispatch(setLoginUrl(get(this.props, "location.query.callbackUrl", `${window.location.protocol}//${window.location.host}`)))
+  }
+
   handleLogin = ({ email }) => {
-    const loginUrl = `${window.location.protocol}//${window.location.host}`
-    this.props.dispatch(generateLink({ email, loginUrl }))
+    const { loginUrl, domain } = this.props
+    this.props.dispatch(generateLink({ domain, email, loginUrl }))
   }
 
   render() {
--- src/ducks/magic-link.js
+++ src/ducks/magic-link.js
@@ -5,12 +5,14 @@ import { createAction, createReducer } from 'redux-act'
 export const generateLinkRequest = createAction('links/request')
 export const generateLinkSuccess = createAction('links/success')
 export const generateLinkFailure = createAction('links/failure')
+export const setDomainAction = createAction('links/set/domain')
+export const setLoginUrlAction = createAction('links/set/loginUrl')
 
-export const generateLink = ({ email, loginUrl }) => {
+export const generateLink = ({ domain, email, loginUrl }) => {
   return dispatch => {
-    dispatch(generateLinkRequest({ email, loginUrl }))
+    dispatch(generateLinkRequest())
     superagent
-      .post(`http://localhost:3333/links`)
+      .post(`https://magic-link.${domain}/links`)
       .send({ email, loginUrl })
       .set("Accept", "application/json")
       .set("Content-Type", "application/json")
@@ -28,9 +30,24 @@ export const generateLink = ({ email, loginUrl }) => {
   }
 }
 
+export const setDomain = (domain) => {
+  return dispatch => {
+    dispatch(setDomainAction(domain))
+  }
+}
+
+export const setLoginUrl = (loginUrl) => {
+  return dispatch => {
+    dispatch(setLoginUrlAction(loginUrl))
+    return
+  }
+}
+
 const initialState = {
   busy: false,
   error: null,
+  domain: 'octoblu.com',
+  loginUrl: null,
 }
 
 export default createReducer({
@@ -39,14 +56,18 @@ export default createReducer({
     busy: true,
     error: null,
   }),
-  [generateLinkSuccess]: (state) => ({
-    ...state,
-    busy: false,
-    error: null,
-  }),
+  [generateLinkSuccess]: () => initialState,
   [generateLinkFailure]: (state, error) => ({
     ...state,
     busy: false,
     error: error,
+  }),
+  [setDomainAction]: (state, domain) => ({
+    ...state,
+    domain: domain || initialState.domain,
+  }),
+  [setLoginUrlAction]: (state, loginUrl) => ({
+    ...state,
+    loginUrl,
   }),
 }, initialState)
