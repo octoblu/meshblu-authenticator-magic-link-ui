26b34b27d14d1419a46d0106fe43819bb2ab9d45
amirmocto / peterdemartini <amirm@citrix.com , thepeterdemartini@gmail.com>
2017-05-15 12:16:18 -0700

    v2.0.3 use new build process

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -58,20 +58,6 @@ module.exports = {
         minifyURLs: true
       }
     }),
-    // Minify the code.
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        screw_ie8: true, // React doesn't support IE8
-        warnings: false
-      },
-      mangle: {
-        screw_ie8: true
-      },
-      output: {
-        comments: false,
-        screw_ie8: true
-      }
-    }),
   ],
   module: {
     rules: [
