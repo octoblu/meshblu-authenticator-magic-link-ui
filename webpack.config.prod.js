057b75342757dd394e66b367cae9ad8c33d3ed36
amirmocto / peterdemartini <amirm@citrix.com , thepeterdemartini@gmail.com>
2017-05-15 13:47:37 -0700

    v2.0.5 fixed webpack config and site title

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,7 @@
-var path         = require('path');
-var webpack      = require('webpack');
+var path              = require('path');
+var webpack           = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var PKG_VERSION = require('./package.json').version
+var PKG_VERSION       = require('./package.json').version
 
 // Assert this just to be safe.
 // Development builds of React are slow and not intended for production.
@@ -15,19 +15,15 @@ module.exports = {
     './src/index'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  output: {
     // The build folder.
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, "dist"),
     // Generated JS file names (with nested folders).
     // There will be one main bundle, and one file per asynchronous chunk.
     // We don't currently advertise code splitting but Webpack supports it.
-    filename: 'js/[name].js',
-    chunkFilename: 'js/[name].[chunkhash:8].chunk.js',
+    filename: "js/[name].js",
+    chunkFilename: "js/[name].[chunkhash:8].chunk.js",
     // We inferred the "public path" (such as / or /my-project) from homepage.
-    publicPath: '/v' + PKG_VERSION
+    publicPath: "/"
   },
   resolve: {
     extensions: ['.js', '.jsx', '.json'],
